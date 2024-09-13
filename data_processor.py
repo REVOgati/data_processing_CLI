fe0bb40b3d91250e7b97b009dc821819a0b4b508
Kimutai101 <tiropgarreth@gmail.com>
2024-09-13 19:57:56 +0300

    Finished writing code for the functions and argument parser

--- data_processor.py
+++ data_processor.py
@@ -34,3 +34,30 @@ def process_data(data, filter_column, threshold):
     aggregated_data = filtered_data.groupby('Month').sum()
     return aggregated_data
 
+#Using argparse to enable user input in CLI
+
+def main():
+    my_parser = argparse.ArgumentParser(description= "Data Cleaning and Processing Tool")
+    my_parser.add_argument('input_file', help= "Path to input CSV file")
+    my_parser.add_argument('output_file', help="Path to save output file")
+    my_parser.add_argument('--filter_column', help='Option to define filter column', default=None)
+    my_parser.add_argument('--threshold', help='Option for threshhold', default= 0)
+    
+    my_args = my_parser.parse_args()
+    
+    data = read_csv_file(my_args.input_file)
+    
+    if data is not None:
+        cleaned_data = clean_data(data)
+        processed_data = process_data(cleaned_data, my_args.filter_column, my_args.threshold)
+        #save transformed data into a new csv_file
+        processed_data.to_csv(my_args.output_file, index= False)
+        
+        printf("Data Processing complete. Saved under {my_args.output_file}")
+        
+""" To ensure that the main() function only runs when run directly
+and Not when imported as a module in another python program """
+
+if __name__ = "__main__":
+    main()
+
